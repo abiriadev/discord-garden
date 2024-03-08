5a5035da6329ccb8fc46639469bdda15eb8b6cfb
Abiria <abiria.dev@gmail.com>
2024-03-08 22:33:25 +0900

    feat: implement rank query

--- lib/lib.go
+++ lib/lib.go
@@ -19,6 +19,34 @@ func Record(wapi api.WriteAPIBlocking, id string, point int, when time.Time) {
 	}
 }
 
+func Rank(qapi api.QueryAPI) map[string]int {
+	res, err := qapi.Query(
+		context.Background(),
+		`from(bucket: "hello")
+			|> range(start: -6d)
+			|> filter(fn: (r) => r["_measurement"] == "chat")
+			|> group(columns: ["id"])
+			|> count()
+			|> group()
+			|> sort(columns: ["_value"], desc: true)`,
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	rankMap := map[string]int{}
+
+	for res.Next() {
+		rankMap[res.Record().ValueByKey("id").(string)] = res.Record().Value().(int)
+	}
+
+	if res.Err() != nil {
+		panic(res.Err())
+	}
+
+	return rankMap
+}
+
 func InitClient(
 	addr string,
 	token string,
