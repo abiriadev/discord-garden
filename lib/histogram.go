deedaf7f85657d4d7aa41d67ae10672b64b13fb4
Abiria <abiria.dev@gmail.com>
2024-03-18 06:58:51 +0900

    feat: add auto histogram mapper

--- lib/histogram.go
+++ lib/histogram.go
@@ -1,5 +1,15 @@
 package lib
 
+import "github.com/samber/lo"
+
 type Histogram interface {
 	Process(data []int, height int) func(int) int
+}
+
+func ApplyHistogram(data []int, height int, histogram Histogram) []int {
+	cb := histogram.Process(data, height)
+
+	return lo.Map(data, func(v, _ int) int {
+		return cb(v)
+	})
 }
