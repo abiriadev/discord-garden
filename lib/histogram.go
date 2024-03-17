661e89e4ebcc9f226bdc0a5bc3d381ca05dede1a
Abiria <abiria.dev@gmail.com>
2024-03-18 07:15:03 +0900

    feat: implement `BinaryMeanHistogram`

--- lib/histogram.go
+++ lib/histogram.go
@@ -1,6 +1,10 @@
 package lib
 
-import "github.com/samber/lo"
+import (
+	"slices"
+
+	"github.com/samber/lo"
+)
 
 type Histogram interface {
 	Process(data []int, height int) func(int) int
@@ -12,4 +16,22 @@ func ApplyHistogram(data []int, height int, histogram Histogram) []int {
 	return lo.Map(data, func(v, _ int) int {
 		return cb(v)
 	})
+}
+
+type BinaryMeanHistogram struct{}
+
+func (_ BinaryMeanHistogram) Process(data []int, height int) func(int) int {
+	tmp := lo.Filter(data, func(item int, _ int) bool {
+		return item > 0
+	})
+	l := len(tmp)
+	slices.Sort(tmp)
+
+	return func(v int) int {
+		if v <= 0 {
+			return v
+		}
+		i, _ := slices.BinarySearch(tmp, v)
+		return i * height / l
+	}
 }
